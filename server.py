7884033c5f7d83c2ced7a9f3f69e2057d339251d
clarence <clarencejames@live.com.au>
2022-11-03 23:53:01 +1100

    intial dev environment setup

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+# import bcrypt
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+app.run(debug=True)
